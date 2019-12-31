02db202c81b0e6134fc9a6b375be1f70b3ba92ad
Oliver Erxleben <oliver.erxleben@me.com>
2019-12-31 12:36:27 +0100

    wip

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,22 +1,41 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+const apikey: admin.ServiceAccount = {
+  projectId: functions.config().service_acc.projectid,
+  clientEmail: functions.config().service_acc.clientemail,
+  privateKey: functions.config().service_acc.key
+}
+
+admin.initializeApp({
+  credential: admin.credential.cert(apikey),
+  databaseURL: "https://squirrel-news-789fd.firebaseio.com"
+});
+
 
 /**
- * used to 
+ * used to store a specific publication into firebase store from a specific client 
  */
-export const publishCurations = functions.https.onRequest( (request, response ) => {
-
+export const publishCurations = functions.https.onRequest( async (request, response ) => {
+  
   if (request.method !== 'POST') {
     response.status(405).json({
       message: "Method is not allowed here."
     })
   } else {
-    response.status(200).json({
-      message: "needs implementation"
-    })
+    const query = await admin.firestore().collection('access-tokens').doc('sheet').get()
+      
+    if (query.get('token') === request.headers['token']) {
+      // TODO: parse request body and insert into firestore 
+      response.status(200).json({
+        "message": "swaggy"
+      });
+    } else {
+      response.status(400).json({
+        "message": "bad request"
+      });
+    }
+
   }
 
 })
