50094f7460fefd6d09504490a4b2552c2b665ac3
Oliver Erxleben <oliver.erxleben@me.com>
2020-03-09 23:21:38 +0100

    now in taps!

--- ng-app/src/app/app-routing.module.ts
+++ ng-app/src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
-    path: 'archive',
-    loadChildren: () => import('./archive/archive.module').then( m => m.ArchivePageModule)
-  },
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  }
 ];
 
 @NgModule({
---
+++ ng-app/src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../home/home.module').then( module => module.HomePageModule )
+          }
+        ]
+      },
+      {
+        path: 'archiv',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../archive/archive.module').then( module => module.ArchivePageModule )
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
