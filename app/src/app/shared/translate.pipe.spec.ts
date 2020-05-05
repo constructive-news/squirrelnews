bd56e6707540d30caf6bf2256b72ff7b4024dfac
Oliver Erxleben <oliver.erxleben@me.com>
2020-05-05 13:33:01 +0200

    cleanuo

--- app/src/app/shared/translate.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TranslatePipe } from './translate.pipe';
-
-describe('TranslatePipe', () => {
-  xit('create an instance', () => {
-    const pipe = new TranslatePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
